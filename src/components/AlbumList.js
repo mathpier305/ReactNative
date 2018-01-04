78afad68ee771e6a3eaa6d8d1a459064d5d54d87
Pierre, Mathurin <mathurin.pierre@cengage.com>
2018-01-03 22:07:23 -0500

    completed taylor swift album

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
+  state ={
+    albums: []
+  };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList ');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+     return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
+    console.log(this.state);
     return (
-      <View>
-        <Text> Album List !!! </Text>
-      </View>
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
